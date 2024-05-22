782d56da23e3519ef93bb2ea3b14c8e99c4507b2
HarshTechnomark <harsh.dhaduk@technomark.io>
2024-05-22 09:59:28 +0530

    updated code for style

--- src/components/settings/tables/Constants/Tabname.ts
+++
@@ -1,7 +0,0 @@
-export const CLIENT = "Client";
-export const PROJECT = "Project";
-export const USER = "User";
-export const PROCESS = "Process";
-export const GROUP = "Group";
-export const STATUS = "Status";
-export const ORGANIZATION = "Organization";
