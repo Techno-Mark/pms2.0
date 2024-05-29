8babdf5b75e2cb7e18f0c899f68cad78c486203b
HarshTechnomark <harsh.dhaduk@technomark.io>
2024-05-29 13:38:42 +0530

    help page changes

--- src/app/help/page.tsx
+++ src/app/help/page.tsx
@@ -16,7 +16,7 @@ const Page = () => {
         </div>
       </div>
       <div className="mx-4 px-5 flex justify-between items-center w-[97%] h-16 bg-whiteSmoke rounded-lg shadow-xl">
-        <span>PMS Executable (.exe) File [Version 2.0] - May 3, 2024</span>
+        <span>PMS Executable (.msi) File [Version 2.0] - May 3, 2024</span>
         <Button variant="contained" className="!bg-secondary">
           <a href="https://pmsaea8.blob.core.windows.net/prod-help-video-setup/PGCTimeTracker.msi">
             Download
@@ -25,27 +25,27 @@ const Page = () => {
       </div>
       <div className="mt-8 mx-4 px-5 flex flex-col justify-between items-start w-[97%]">
         <strong className="py-1">
-          Important Notes for PMS Executable (.exe) File:
+          Important Notes for PMS Executable (.msi) File:
         </strong>
         <br />
         <ol className="pl-5 !list-decimal flex flex-col gap-1">
           <li className="pb-1">
-            Each user must install the PMS Executable File (.exe) with
+            Each user must install the PMS Executable File (.msi) with
             assistance from the IT team.
           </li>
           <li className="pb-1">
             Ensure that the user system has the most recent version of the PMS
-            Executable (.exe) File [Version 2.0] - May 3, 2024 and remove any
+            Executable (.msi) File [Version 2.0] - May 3, 2024 and remove any
             outdated version if any.
           </li>
           <li className="pb-1">
             In the event of a password change on the PMS web portal, it is
             essential to update the password within the PMS Executable File
-            (.exe) as well.
+            (.msi) as well.
           </li>
           <li className="pb-1">
             Each user is required to ensure that auto-login functions smoothly
-            on a daily basis at PMS Executable File (.exe). In the event of any
+            on a daily basis at PMS Executable File (.msi). In the event of any
             issues, users should promptly contact the IT team for support.
           </li>
           <li className="pb-1">
