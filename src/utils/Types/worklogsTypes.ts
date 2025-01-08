89b14fd89d34e04c5ea8f75125a27ef8129f6c69
HarshTechnomark <127918058+HarshTechnomark@users.noreply.github.com>
2025-01-08 10:56:02 +0530

    error log field sequence changes

--- src/utils/Types/worklogsTypes.ts
+++ src/utils/Types/worklogsTypes.ts
@@ -287,9 +287,9 @@ export interface ErrorlogGetByWorkitem {
   isSolved?: boolean;
   DisableErrorLog?: boolean;
   IsHasErrorlogAddedByClient?: boolean;
-  // ErrorIdentificationDate: string;
-  // ResolutionStatus: number;
-  // IdentifiedBy: string | null;
+  ErrorIdentificationDate: string;
+  ResolutionStatus: number;
+  IdentifiedBy: string | null;
 }
 
 export interface ReviewerNoteDetails {
