26909733b74fe749b05b8ccd490917552a063b8e
HarshTechnomark <127918058+HarshTechnomark@users.noreply.github.com>
2025-02-19 09:47:33 +0530

    new changes for top and left

--- src/components/emailbox/SubjectPopup.tsx
+++ src/components/emailbox/SubjectPopup.tsx
@@ -55,7 +55,6 @@ const SubjectPopup: React.FC<SubjectPopupProps> = ({
   useEffect(() => {
     if (feedbackRef.current) {
       const rect = feedbackRef.current.getBoundingClientRect();
-      console.log(rect);
       setFeedbackPosition({
         top:
           rect.top -
